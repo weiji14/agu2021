444e2166773e121bfcf2e87e878b029bf9e8f15d
Wei Ji <weiji.leong@vuw.ac.nz>
2021-01-19 00:16:57 +1300

    :boom: Map of Siple Coast active subglacial lakes
    Taking a moment to savour a beautiful map! Shiny PyGMT code that layers:
    
    1. MOA and MEaSUREs velocity basemaps
    2. Longitude/Latitude graticules and Scripps grounding line
    3. DeepIceDrain active subglacial lakes polygons (currently using v0.4.0)
    4. Siple Coast placename and lake labels (painstakingly created by hand)
    5. A colorbar and inset map for context
    
    Again, this was heavily inspired by https://github.com/mrsiegfried/Venturelli2020-GRL, but there's some advanced Python/PyGMT code going on too. Will need to commit the 'antarctic_subglacial_lakes_3031.geojson' to 'antarctic_subglacial_lakes_3031.gmt' code some day. Not rendering the jupyter notebook cells yet, only showing a teaser image on the main README.md.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Scripts for making [PyGMT](https://github.com/GenericMappingTools/pygmt) figures for the [2021 Antarctic Science Conference](https://www.antarcticanz.govt.nz/nzasc21/antarctic-science-conference) happening in Christchurch, New Zealand from 9-12 February 2021.
 
+**Title**: Spatiotemporal variability of active subglacial lakes in Antarctica from 2018-2020 using ICESat-2 laser altimetry
+
+![Siple Coast active subglacial lakes](https://user-images.githubusercontent.com/23487320/104906156-36b44280-59e8-11eb-8be2-1823d3dbf344.png)
+
 ## Quickstart
 
 Launch in [Pangeo Binder](https://pangeo-binder.readthedocs.io) (Interactive jupyter lab environment in the cloud).
--- key_figure.py
+++ key_figure.py
@@ -24,7 +24,7 @@
 
 # %%
 import os
-from glob import glob
+import re
 
 import geopandas as gpd
 import numpy as np
@@ -94,5 +94,236 @@ except AssertionError:
             module="grdmath",
             args=f"{vel_file}.nc?VX 2 POW {vel_file}.nc?VY 2 POW POW 0.5 = {vel}",
         )
+
+# %%
+# DeepIceDrain active subglacial lake outlines
+lakes = "https://raw.githubusercontent.com/weiji14/deepicedrain/v0.4.0/antarctic_subglacial_lakes_3031.geojson"
+
+
+# %% [markdown]
+# ### Make color maps
+#
+# For MOA and ice velocity and vertical elevation trend (dhdt).
+# Scientific color maps are from http://doi.org/10.5281/zenodo.1243862
+
+# %%
+pygmt.makecpt(
+    series=[15000, 17000, 1],
+    cmap="grayC",
+    continuous=True,
+    output="cmap_moa.cpt",
+    reverse=True,
+)
+with pygmt.config(COLOR_FOREGROUND="240/249/33", COLOR_BACKGROUND="13/8/135"):
+    pygmt.makecpt(series=[0, 1000, 1], cmap="batlow", output="cmap_vel.cpt")
+pygmt.makecpt(
+    cmap="berlin",
+    series=[-3, 3, 1],
+    reverse=True,
+    continuous=True,
+    output="cmap_dhdt.cpt",
+)
+
+# %%
+
+# %% [markdown]
+# # Figure of Siple Coast active subglacial lakes
+
+# %%
+# We're making this a specific height
+figheight = 115  # in mm
+
+# Region in PS71 for main part of the figure
+sip_xl, sip_xh, sip_yl, sip_yh = sipreg = [-800_000, 25_000, -1_000_000, -400_000]
+
+# Calculate the figure width and map scale
+figwidth = figheight * (sip_xh - sip_xl) / (sip_yh - sip_yl)
+sipratio = (sip_yh - sip_yl) / (figheight / 1000)
+
+# Make a GMT region string and projection strings in both PS71 and Lon/Lat
+sipreg = [sip_xl, sip_xh, sip_yl, sip_yh]
+sipproj = f"x1:{sipratio}"
+sipproj_ll = f"s0/-90/-71/1:{sipratio}"
+
+# Region for Whillans Grounding Zone inset
+wgz_xl, wgz_xh, wgz_yl, wgz_yh = wgzreg = [-185_000, -627_500, -145_000, -580_000]
+
+# Make GMT projection strings (only need PS71 because no graticules)
+wgzratio = sipratio / 7  # 7x zoom in for the WGZ inset seems to work well
+wgzmap = f"x1:{wgzratio}"
+
+# %%
+# Initialize figure and plot MOA as the basemap with ticks every 200 km in xy directions
+fig = pygmt.Figure()
+with pygmt.config(MAP_FRAME_TYPE="inside"):
+    fig.basemap(
+        region=sipreg, projection=sipproj, frame=["nwse", "xf200000", "yf200000"]
+    )
+    fig.grdimage(grid=moa, cmap="cmap_moa.cpt", nan_transparent=True)
+
+# Plot graticules overtop, every 2° latitude and 15° longitude
+with pygmt.config(
+    MAP_ANNOT_OFFSET_PRIMARY="-2p",
+    MAP_FRAME_TYPE="inside",
+    MAP_ANNOT_OBLIQUE=0,
+    FONT_ANNOT_PRIMARY="8p,grey",
+    MAP_GRID_PEN_PRIMARY="grey",
+    MAP_TICK_LENGTH_PRIMARY="-10p",
+    MAP_TICK_PEN_PRIMARY="thinnest,grey",
+    FORMAT_GEO_MAP="dddF",
+    MAP_POLAR_CAP="88/90",  # less longitude graticules at >88°S
+):
+    fig.basemap(
+        projection=sipproj_ll, region=sipreg, frame=["NSWE", "xa15g15", "ya2g2"]
+    )
+
+# Plot the grounding line in white
+fig.plot(data=groundingline, region=sipreg, projection=sipproj, pen="0.15p,white")
+
+
+# %%
+# Overlay ice velocity with 70% transparency
+fig.grdimage(grid=vel, cmap="cmap_vel.cpt", transparency=70, nan_transparent=True)
+# Overlay dhdt with 30% transparency
+# pygmt.makecpt(cmap="berlin", series=[-1.0, 1.0, 0.25], continuous=True, reverse=True)
+# fig.grdimage(
+#     grid="ATLXI/ds_grid_dhdt_siple_coast.nc",
+#     cmap=True,
+#     # cmap="cmap_dhdt.cpt",
+#     transparency=30,
+#     nan_transparent=True,
+# )
+fig.show()
+
+
+# %%
+# Plot lakes in PS71 as blobs (red for draining, blue for filling)
+# TODO refactor after pygmt/geopandas integration is done,
+# see https://github.com/GenericMappingTools/pygmt/issues/608
+with pygmt.helpers.GMTTempFile(suffix=".gmt") as tmpfile:
+    os.remove(path=tmpfile.name)
+    gpd.read_file(lakes).to_file(tmpfile.name, driver="OGR_GMT")
+    fig.plot(
+        data=tmpfile.name,  # "antarctic_subglacial_lakes_3031.geojson"
+        pen="thinnest,yellow,-",
+        cmap="cmap_dhdt.cpt",
+        color="+z",
+        close=True,
+        # transparency=30,
+        a="Z=inner_dhdt",
+    )
+
+# %%
+# Siple Coast placename labels
+gdf = gpd.read_file("antarctic_subglacial_lakes_3031.gmt")
+with open("place_labels_siple_coast.txt", mode="w") as file:
+    # Ice Streams A to E
+    font = "7p,Helvetica-Narrow-Oblique,white"
+    print(f"-320000\t-440000\t-45\t{font}\tCM\tMercer Ice Stream", file=file)
+    print(f"-370000\t-555000\t-5\t{font}\tCM\tWhillans Ice Stream", file=file)
+    print(f"-470000\t-450000\t-55\t{font}\tCM\tVan der Veen Ice Stream", file=file)
+    print(f"-750000\t-450000\t-58\t{font}\tCM\tKamb Ice Stream", file=file)
+    print(f"-700000\t-700000\t-45\t{font}\tCM\tBindschadler Ice Stream", file=file)
+    print(f"-700000\t-850000\t-37\t{font}\tCM\tMacAyeal Ice Stream", file=file)
+
+    # Ice ridges, rises and domes
+    print(f"-370000\t-480000\t-30\t{font}\tCM\tConway Ice Ridge", file=file)
+    print(f"-400000\t-600000\t0\t{font}\tCM\tEngelhardt Ice Ridge", file=file)
+    print(f"-100000\t-750000\t-45\t{font}\tCM\tCrary Ice Rise", file=file)
+    print(f"-450000\t-780000\t-35\t{font}\tCM\tSiple Dome", file=file)
+    print(f"-650000\t-800000\t-25\t{font}\tCM\tShabtaie Ice Ridge", file=file)
+    print(f"-650000\t-950000\t-15\t{font}\tCM\tHarrison Ice Ridge", file=file)
+
+    # Abbreviated lakes
+    abbrev_dict: dict = {
+        "Subglacial": "S",
+        "Lake": "L",
+        "Conway": "C",
+        "Engelhardt": "E",
+        "Kamb": "K",
+        "MacAyeal": "Mac",
+        "Mercer": "M",
+        # "Recovery", "R",
+        # "Slessor","S"
+        "Whillans": "W",
+    }
+    pattern = re.compile(pattern=rf"\b({'|'.join(abbrev_dict.keys())})\b")
+
+    for idx, row in gdf.dissolve(by="lake_name", as_index=False).iterrows():
+        x, y = row.geometry.centroid.xy
+
+        label = pattern.sub(
+            repl=lambda name: abbrev_dict[name.group()], string=row.lake_name
+        ).replace(" ", "")
+        justify = (
+            "BL" if label in ("L78", "SLM", "SLW") else "TC" if "*" in label else "TR"
+        )
+
+        print(f"{x[0]:.0f}\t{y[0]:.0f}\t0\t6p,white\t{justify}\t{label}", file=file)
+
+# %%
+# Plot labels for Siple Coast ice streams, active subglacial lakes, etc
+fig.text(
+    textfiles="place_labels_siple_coast.txt",
+    angle=True,
+    font=True,
+    justify=True,
+    offset="j0.12c",
+    # frame=["WsNe", "af10000g50000"],
+)
+fig.show()
+
+# %%
+# Plot the color bar once with a transparent box, then again with no box and no transparency
+with pygmt.config(
+    FONT_ANNOT_PRIMARY="6p,white",
+    FONT_LABEL="6p,white",
+    MAP_ANNOT_OFFSET_PRIMARY="2p",
+    MAP_TICK_PEN_PRIMARY="0.5p,white",
+    MAP_TICK_LENGTH_PRIMARY="3p",
+    MAP_FRAME_PEN="0.5p,white",
+    MAP_LABEL_OFFSET="4p",
+):
+    colorbar_kwargs = dict(
+        cmap="cmap_dhdt.cpt",
+        position="jBR+jBR+w1.6c/0.18c+o1.2c/0.3c+v+e",
+        frame=["xaf", 'y+l"dhdt (m/yr)"'],
+    )
+    fig.colorbar(
+        box="+gblack+c-9p/3p",
+        # box = '+gblack+p0.5p,black+c3p'
+        transparency=70,
+        **colorbar_kwargs,
+    )
+    fig.colorbar(**colorbar_kwargs)
+
+fig.show()
+
+# %%
+# Make insets of Antarctica
+# Workaround until https://github.com/GenericMappingTools/pygmt/pull/788 is merged
+antwidth = 3  # width of inset in cm
+with pygmt.clib.Session() as lib:
+    lib.call_module(module="inset", args=f"begin -DjTR+w{antwidth}c")
+
+    # Plot the inset map
+    fig.basemap(region=vel, projection=f"S0/-90/71/{antwidth}c", frame="+n")
+    fig.coast(area_thresh="+a", land="white")  # ice shelf in white
+    fig.coast(area_thresh="+ag", land="gray")  # grounded ice in gray
+    fig.plot(
+        projection=f"X{antwidth}c",
+        x=[sip_xl, sip_xl, sip_xh, sip_xh, sip_xl],
+        y=[sip_yl, sip_yh, sip_yh, sip_yl, sip_yl],
+        pen="1p,black",  # map location in black
+    )
+
+    lib.call_module(module="inset", args="end")
+
+fig.show()
+
+# %%
+# Save the figure
+fig.savefig(fname="siple_coast_lakes.pdf")
+fig.savefig(fname="siple_coast_lakes.png")
 
 # %%
